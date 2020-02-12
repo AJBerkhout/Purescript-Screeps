dcefb68733b8fd4c8b0106b0c4d111e6462b3e63
Alex B <alexander.berkhout@marquette.edu>
2020-02-12 08:49:40 -0600

    Updated version to %system.version%

---
+++ src/backend/Version.fs
@@ -0,0 +1,14 @@
+﻿namespace GasDay.WebPlatform
+
+open System.Diagnostics.CodeAnalysis
+
+[<ExcludeFromCodeCoverage>]
+type Version =
+  {
+    Version: string
+  }
+
+  static member Default = 
+    {
+      Version = "1.8.91.4"
+    }
